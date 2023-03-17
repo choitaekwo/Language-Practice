a1cd38c625994716012643e31dd7aeddac3fa3cf
TaekwonChoi <107266687+choitaekwo@users.noreply.github.com>
2023-03-17 21:14:18 +0900

    Create HelloWorld.java

---
+++ Java-Practice/Chap_01/HelloWorld.java
@@ -0,0 +1,10 @@
+package chap_01;
+
+public class _01_HelloWorld {
+	public static void main (String[] args) {
+		System.out.println("HelloWorld!!!!!!!");
+		System.out.println("안녕");
+		System.out.println("싫어");
+	}
+
+}
