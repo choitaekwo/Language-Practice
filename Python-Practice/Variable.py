f20173c16b7bfbc881ee8debe054fedfec5aab4b
TaekwonChoi <107266687+choitaekwo@users.noreply.github.com>
2023-03-17 21:06:06 +0900

    Create Variable.py

---
+++ Python-Practice/Variable.py
@@ -0,0 +1,14 @@
+animal = "강아지"
+name = "연탄"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+print("우리집 "+ animal + "의 이름은 " + name + "이에요.")
+print(name + "이는 " + str(age) + "살이며 " + hobby + "을 아주 좋아해요.")      
+print(name + "이는 어른일까요? " + str(is_adult))      
+     
+station = input("XX행: ")
+print(f"{station}행 열차가 들어오고 있습니다.")
+
+
+print(oct(47))
