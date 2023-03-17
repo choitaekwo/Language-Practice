9468fde6b39408e725891592517b1b60396a9883
TaekwonChoi <107266687+choitaekwo@users.noreply.github.com>
2023-03-17 21:16:34 +0900

    Add files via upload

---
+++ Java-Practice/Chap_01/_07_TypeCasting.java
@@ -0,0 +1,50 @@
+package chap_01;
+
+public class _07_TypeCasting {
+	public static void main(String [] args) {
+		
+		int score = 93;
+		System.out.println(score);  // 93
+		System.out.println((float)score);  // 93.0
+		System.out.println((double)score); // 93.0
+		
+		
+		float score_f = 93.3F;
+		double score_d = 98.8;
+		System.out.println((int) score_f); // 93
+		System.out.println((int) score_d); // 98
+		
+		
+		// 정수 + 실수 연산
+		score = 93 + (int)98.8;  // 93 + 98
+		System.out.println(score); // 191
+		
+		score_d = (double)93 + 98.8; // 93.0 + 98.8
+		System.out.println(score_d); // 191.8
+		
+		
+		// 변수에 형 변환된 데이터 집어넣기
+		double ConvertedScoreDouble = score;  // 191 -> 191.0
+		// int -> long -> float -> double (자동 형변환) 큰 쪽으로
+		
+		int ConvertedScoreInt = (int)score_d; // 191.8 -> 191
+		// double -> float -> long -> int (수동 형변환) 작은 쪽으로
+		
+		// 숫자를 문자열로
+		String s1 = String.valueOf(93);
+		s1 = Integer.toString(93);
+		System.out.println(s1);
+		
+		String s2 = String.valueOf(98.8);
+		s2 = Double.toString(98.8);
+		System.out.println(s2);
+		
+		// 문자열을 숫자로
+		int i = Integer.parseInt("93");
+		System.out.println(i);
+		double d = Double.parseDouble("98.8");
+		System.out.println(d);
+		
+	}
+
+}
---
+++ Java-Practice/Chap_01/_Quiz_01.java
@@ -0,0 +1,17 @@
+package chap_01;
+
+public class _Quiz_01 {
+	public static void main(String[] args) {
+		
+		  String BusNumber = "상암08"; // 버스 번호 
+		  double leftDistance = 1.2; // 남은 거리
+		  int leftTime = 3;          // 남은 시간
+		  
+		  // 결과 출력
+		  System.out.println(BusNumber + "번 버스"); 
+		  System.out.println("약 " + leftTime + "분 후 도착"); 
+		  System.out.println("남은 거리 " + leftDistance + "km");
+		 
+	}
+
+}
